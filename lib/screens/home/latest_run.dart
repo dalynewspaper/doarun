49d6b6e36e700d02962dd96999ae4a3c31b2ff33
qch.jeff <jeffson.quach@epitech.eu>
2021-04-16 15:15:33 +0200

    feat: added latest runs title design

--- lib/screens/home/latest_run.dart
+++ lib/screens/home/latest_run.dart
@@ -1,9 +1,21 @@
+import 'package:doarun/style/color.dart';
 import 'package:doarun/style/text.dart';
 import 'package:flutter/cupertino.dart';
 
 class LatestRun extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("Latest runs".toUpperCase(), style: textStyleH1);
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              color: mainColor,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(15.0, 15.0, 25.0, 15.0),
+                child: Text("Latest runs".toUpperCase(), style: textStyleTitle),
+              )
+          ),
+        ]
+    );
   }
 }
