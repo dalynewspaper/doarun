c21007d2f89fa4d7ce3c80d0f94e05810c94f3e0
Mark <mark.alexander.pro@gmail.com>
2021-05-14 16:50:25 +0200

    feat: group creation

---
+++ lib/screens/profile/create_group/create_group.dart
@@ -0,0 +1,47 @@
+import 'package:doarun/states/group_states.dart';
+import 'package:doarun/style/color.dart';
+import 'package:doarun/urls.dart';
+import 'package:doarun/utils/database/entities/group/entity_group.dart';
+import 'package:doarun/widgets/group_creation.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CreateGroup extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _CreateGroup();
+}
+
+class _CreateGroup extends State<CreateGroup> {
+  final GroupStates groupStates = Get.find();
+
+  @override
+  void initState() {
+    groupStates.group.value = EntityGroup();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        iconTheme: IconThemeData(
+          color: mainColor, //change your color here
+        ),
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              groupStates.group.value = groupStates.groupsOwned.first;
+              Get.offNamed(URL_PROFILE);
+            }),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: GroupCreation(isOnboarding: false),
+      ),
+    );
+  }
+}
