b108baa69fc0db57ae2394fcb0f501b05ce36585
Mark <mark.alexander.pro@gmail.com>
2021-07-01 17:27:30 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $# -lt 2 || ($1 != "web" && $1 != "android") || ($2 != "beta" && $2 != "production") ]]
+then
+  printf "Please pass 'web' or 'android' as a first argument and 'beta' or 'production' as a second argument.\n"
+elif [[ $2 == "production" ]]
+then
+  printf "Production flavor is not available yet.\n"
+elif [[ $1 == "web" ]]
+then
+  rm ./web/index.html
+  cp ./web/flavors/index_"$2".html ./web
+  mv ./web/index_"$2".html ./web/index.html
+  flutter build "$2" -t ./lib/main_"$2".dart --flavor "$2"
+elif [[ $1 == "android" ]]
+then
+  rm ./android/app/src/main/AndroidManifest.xml
+  cp ./android/flavors/AndroidManifest_"$2".xml ./android/app/src/main/
+  mv ./android/app/src/main/AndroidManifest_"$2".xml ./android/app/src/main/AndroidManifest.xml
+  flutter build apk --flavor "$2" -t ./lib/main_"$2".dart
+fi
